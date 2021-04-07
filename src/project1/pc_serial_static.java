8a2f1c5478b0dcd4a0298e229a3e858504f2de89
kshmawj111 <45940359+kshmawj111@users.noreply.github.com>
2021-04-08 02:49:39 +0900

    load balancing considering number size

--- src/project1/pc_serial_static.java
+++ src/project1/pc_serial_static.java
@@ -1,35 +1,35 @@
 package project1;
 import java.lang.Thread;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 class det_prime extends Thread{
     int num_of_primes = 0;
     long total_milliseconds = 0;
-    int[] numbers;
-    int thread_num =0;
-    int num_total_thread = 0;
+    int start =0;
+    int end = 0;
+    int num_thread=0;
 
-    det_prime(int[] numbers, int thread_num, int num_total_thread){
-        this.numbers = numbers;
-        this.thread_num=thread_num;
-        this.num_total_thread=num_total_thread;
+    det_prime(int start, int end, int num_thread){
+        this.start = start;
+        this.end = end;
+        this.num_thread = num_thread;
     }
 
     public void run(){
         long startTime = System.currentTimeMillis();
         int i;
 
-        for (int x =thread_num; x<numbers.length; x=x+num_total_thread) {
+        for (int x=start; x<end; x=x+num_thread) {
             boolean is_prime = true;
-
-            for (i = 2; i < x/2; i++) {
-                if (x % i == 0) {
-                    is_prime = false;
-                    break;
+            if (x<=1){
+                is_prime=false;
+            }
+            else {
+                for (i = 2; i < x; i++) {
+                    if (x % i == 0) {
+                        is_prime = false;
+                        break;
+                    }
                 }
             }
             total_milliseconds += (System.currentTimeMillis() - startTime); // record execution time
@@ -49,7 +49,6 @@ public class pc_serial_static {
     private static final int NUM_END = 200000;
     private static final int NUM_THREAD = 4;
     private static final det_prime[] threads = new det_prime[NUM_THREAD]; // det_prime 형 threads 배열 생성
-    private static final int[] numbers = new int [NUM_END];
 
     public static void main(String[] args){
         initialize();
@@ -70,11 +69,8 @@ public class pc_serial_static {
     }
 
     private static void initialize(){
-        for (int i=0; i<NUM_END; i++){
-            numbers[i] = i+1;
-        }
         for(int i=0; i<NUM_THREAD; i++){
-            threads[i] = new det_prime(numbers, i, NUM_THREAD);
+            threads[i] = new det_prime(i, NUM_END, NUM_THREAD);
         }
     }
 
@@ -86,7 +82,7 @@ public class pc_serial_static {
             total_primes += threads[i].get_Num_of_primes();
         }
 
-        System.out.println("Total " + total_primes + " prime numbers inbetween 1 and "+ NUM_END);
+        System.out.println("Total " + total_primes + " prime numbers between 1 and "+ NUM_END);
 
     }
 
